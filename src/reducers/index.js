46f639f30048aadc2502780faeb63a9aeedd2e63
myApp <mmichi1139@gmail.com>
2021-06-26 13:14:28 +0900

    implement event creation

---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+// action = {
+//     type: 'CREATE_EVENT',
+//     title: '2021東京オリンピック開催',
+//     body: '2021に東京でオリンピックを開催いたします！コロナのため1年延期されました。'
+// }
+// 　state = []
+// state = [id:1, tile: 'タイトル1' body:'ボディー1'],
+
+const events = (state = [], action) => {
+  switch (action.type) {
+    case 'CREATE_EVENT':
+      const event = { title: action.title, body: action.body };
+      const length = state.length;
+      let id = length === 0 ? 1 : state[length - 1].id + 1;
+
+      return [...state, { id, ...event }];
+    case 'DELETE_EVENT':
+      return state;
+    case 'DELETE_ALL_EVENTS':
+    default:
+      return state;
+  }
+};
+export default events;
