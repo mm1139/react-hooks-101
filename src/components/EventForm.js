7dba334b382367db24a02d3e2379f0722a42286b
myApp <mmichi1139@gmail.com>
2021-07-06 15:45:58 +0900

    delete all operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,12 @@
 import React, { useState, useContext } from 'react';
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions';
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPEATION_LOG,
+  DELETE_ALL_OPEATION_LOGS,
+} from '../actions';
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -17,6 +23,12 @@ const EventForm = () => {
       title,
       body,
     });
+
+    dispatch({
+      type: ADD_OPEATION_LOG,
+      description: 'イベントを作成いたしました。',
+      operatedAt: timeCurrentIso8601(),
+    });
     setTitle('');
     setBody('');
   };
@@ -26,9 +38,26 @@ const EventForm = () => {
     const result = window.confirm(
       '全てのイベントを本当に削除してもいいですか？'
     );
-    if (result) dispatch({ type: DELETE_ALL_EVENTS });
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENTS });
+      dispatch({
+        type: ADD_OPEATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601(),
+      });
+    }
   };
   const unCreatable = title === '' || body === '';
+
+  const deleteAllOperationLogs = (e) => {
+    e.preventDefault();
+    const result = window.confirm(
+      '全ての操作ログを本当に削除してもいいですか？'
+    );
+    if (result) {
+      dispatch({ type: DELETE_ALL_OPEATION_LOGS });
+    }
+  };
 
   return (
     <>
@@ -64,6 +93,12 @@ const EventForm = () => {
           onClick={deleteAllEvents}
           disabled={state.events.length === 0}>
           全てのイベントを削除
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={deleteAllOperationLogs}
+          disabled={state.operationLogs.length === 0}>
+          全ての操作ログを削除
         </button>
       </form>
     </>
--- src/reducers/operationLogs.js
+++ src/reducers/operationLogs.js
@@ -1,14 +1,14 @@
-import { ADD_OPEATION_LOG, DELETE_OPEATION_LOGS } from '../actions';
+import { ADD_OPEATION_LOG, DELETE_ALL_OPEATION_LOGS } from '../actions';
 
 const operationLogs = (state = [], action) => {
   switch (action.type) {
     case ADD_OPEATION_LOG:
       const operationLog = {
         description: action.description,
-        operateAt: action.operateAt,
+        operatedAt: action.operatedAt,
       };
       return [operationLog, ...state];
-    case DELETE_OPEATION_LOGS:
+    case DELETE_ALL_OPEATION_LOGS:
       return [];
     default:
       return state;
