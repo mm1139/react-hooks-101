8bf65548cc1d24f50fefdb81eb3034a5b9a48043
myApp <mmichi1139@gmail.com>
2021-07-07 22:53:37 +0900

    display all operation logs

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import EventForm from './EventForm';
 import Events from './Events';
 import AppContext from '../contexts/AppContext';
 import reducer from '../reducers';
+import OperationLogs from './OperationLogs';
 
 //console.log({ AppContext });
 
@@ -19,6 +20,7 @@ const App = () => {
       <div className="container-fluid">
         <EventForm />
         <Events />
+        <OperationLogs />
       </div>
     </AppContext.Provider>
   );
